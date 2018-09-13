2bc1da11671a905d4c1168fde670b4b6da23cc0e
Stanislav Nikitin <nikitin.develop@gmail.com>
2018-09-13 21:42:30 +0300

    08 Core Concept - Middleware and Error handling

--- my-files/controllers/storeController.js
+++ my-files/controllers/storeController.js
@@ -1,3 +1,13 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Stan';
+  if (req.name === 'Stan') {
+    throw Error(`That is a stupid name: ${req.name}`);
+  }
+  // res.cookie('name', 'Stan is cool', { maxAge: 987654321 });
+  next();
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 }
--- my-files/routes/index.js
+++ my-files/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-router.get('/', storeController.homePage);
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 // :varname
 router.get('/reverse/:name', (req, res) => {
