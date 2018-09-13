c0f241007ffabbf1098c5339bd381dd9cb6b6c40
Stanislav Nikitin <nikitin.develop@gmail.com>
2018-09-14 00:20:53 +0300

    10 Use mixins, create editStore form

--- my-files/routes/index.js
+++ my-files/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 // :varname
 router.get('/reverse/:name', (req, res) => {
