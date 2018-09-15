fb72c5ad7471211a19e68f621c5d89617046e77c
Stanislav Nikitin <nikitin.develop@gmail.com>
2018-09-15 15:40:44 +0300

    14 Create editing flow

--- my-files/controllers/storeController.js
+++ my-files/controllers/storeController.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store'); // It's a Singleton (unique global object)
 
@@ -22,8 +23,40 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 }
 
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+
+  const store = await Store.findOne({ _id: req.params.id });
+
+  // 2. Confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { store, title: `Edit "${store.name}"` });
+}
+
 exports.getStores = async (req, res) => {
   // 1. Query database for a list of all stores
   const stores = await Store.find();
   res.render('stores', { stores, title: 'Stores' });
+}
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update the store
+  /**
+   * findOneAndUpdate(
+   *   query {Object},
+   *   data {Object},
+   *   options {Object}
+   * )
+   */
+  // req.body -- Form data sent by them
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // Return new store instead of old one
+    runValidators: true, // Force the model to run validators before update
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+
+  // 2. Redirect them to the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`)
 }
--- my-files/routes/index.js
+++ my-files/routes/index.js
@@ -8,9 +8,13 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
 
 // :varname
 router.get('/reverse/:name', (req, res) => {
+  console.log(req.params);
   const { name } = req.params;
   // Decompose string to array, reverse it, join back together
   const reversedName = [...name].reverse().join('')
